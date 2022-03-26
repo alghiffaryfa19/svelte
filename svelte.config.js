4b99533c53a89cf33adc2a0b7650418192dff305
Fauzan Amir Al Ghiffary <56854433+alghiffaryfa19@users.noreply.github.com>
2022-03-26 17:22:40 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
-import adapter from'@sveltejs/adapter-netlify';
-  
- const config = {
-   kit: {
-adapter:adapter(),
- 	}
- };
-  
- export default config
+/** @type {import('@sveltejs/kit').Config}*/
+import adapter from '@sveltejs/adapter-netlify'
+const config = {
+        kit: {
+                target: '#svelte',
+                adapter: adapter()
+        }
+};
+
+export default config;
